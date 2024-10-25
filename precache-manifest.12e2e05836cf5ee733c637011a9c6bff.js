11c1649c3218138c2a7efbcce460285951e46b12
Karthik Mungamuru <karthikmns@gmail.com>
2024-10-25 09:31:38 +0000

    Updates

---
+++ precache-manifest.12e2e05836cf5ee733c637011a9c6bff.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b35a7fed3f742a5d466837066654223f",
+    "url": "./index.html"
+  },
+  {
+    "revision": "509bffc3b1d1091d32bc",
+    "url": "./static/js/2.67af74b8.chunk.js"
+  },
+  {
+    "revision": "766a5a47d2d9195b1112",
+    "url": "./static/js/main.17c4a71c.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
